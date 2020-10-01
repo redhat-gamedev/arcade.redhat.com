1d8c7978825e65e2dc4340cbea4521c44d778d5c
mwcz <mwc@clayto.com>
2020-10-01 15:32:05 -0400

    change token storage name, play anim after auth

--- auth.js
+++ auth.js
@@ -5,7 +5,8 @@ window.kc = keycloak;
 keycloak.init().success(async function(authenticated) {
     console.log(`[auth] initialized keycloak.js`);
 
-    const storedToken = await localforage.getItem("jwt");
+    const storedToken = await localforage.getItem("arcade-username");
+    let token = storedToken;
 
     // if there's no stored token, and we aren't authenticated, log in
     if (!storedToken && !authenticated) {
@@ -16,20 +17,20 @@ keycloak.init().success(async function(authenticated) {
     // if we're authenticated but the token hasn't been stored yet
     if (authenticated && !storedToken) {
         console.log(`[auth] logged in, storing user token`);
-        await localforage.setItem("jwt", _.pick(keycloak.tokenParsed, ["email", "preferred_username"]));
+        token = _.pick(keycloak.tokenParsed, ["email", "preferred_username"])
+        await localforage.setItem("arcade-username", token);
     }
 
     // if the token is stored locally, retrieve it and copy the username into a
-    // global var for PAE to pick up
-    if (storedToken) {
-        const email = storedToken.email || "";
-        const name = storedToken.preferred_username || email.replace(/@.*/, "");
-        console.log(`[auth] passing username ${name} to PAE`);
-        if (name) {
-            window.PAE_USERNAME = name;
-        }
-
+    const email = token.email || "";
+    const name = token.preferred_username || email.replace(/@.*/, "");
+    console.log(`[auth] passing username ${name} to PAE`);
+    if (name) {
+        window.ARCADE_USERNAME = name;
+        document.querySelector("#username").innerText = name;
     }
+
+    document.body.removeAttribute("unauthenticated");
 
 }).error(function() {
     console.log('failed to initialize');
