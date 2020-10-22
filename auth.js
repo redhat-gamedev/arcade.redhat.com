8fabeea7efdc5ba1d7f75eab40a1b9e9f37a0bb1
mwcz <mwc@clayto.com>
2020-10-22 10:13:29 -0400

    skip authentication if kerb name is already cached

--- auth.js
+++ auth.js
@@ -6,6 +6,20 @@ keycloak.init().success(async function(authenticated) {
     console.log(`[auth] initialized keycloak.js`);
 
     let name = localStorage.getItem("arcade-username");
+
+    if (name) {
+        // if the token is stored locally, retrieve it and copy the username into a
+        console.log(`[auth] passing username ${name} to PAE`);
+        if (name) {
+            document.querySelector("#username").innerText = name;
+        }
+
+        document.body.removeAttribute("unauthenticated");
+        console.log(`[auth] name cached, no need to auth`);
+        return;
+    }
+
+
     let storedToken;
 
     try {
@@ -49,16 +63,6 @@ keycloak.init().success(async function(authenticated) {
             }
         }
 
-    }
-
-    if (name) {
-        // if the token is stored locally, retrieve it and copy the username into a
-        console.log(`[auth] passing username ${name} to PAE`);
-        if (name) {
-            document.querySelector("#username").innerText = name;
-        }
-
-        document.body.removeAttribute("unauthenticated");
     }
 
 }).error(function() {
